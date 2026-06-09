9b954f3494dd39323d6f1fa6cf7a2c27e5687858
Olyndira <olyndiradjennedora@gmail.com>
2026-06-09 12:46:09 -0300

    initial vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,9 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig();
+
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()]
+})
